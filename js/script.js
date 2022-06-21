320282ba5d6231ccdad54aec212fdc9959af8d32
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-21 14:40:36 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+const list = ['carne', 'pollo', 'uva', 'fagioli', 'olio', 'cipolla'];
+
+const cart = document.getElementById('cart-item');
+console.log(cart);
