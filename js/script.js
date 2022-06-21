c9859ecefe3f70bb226a10effc5041e3911cf4bc
andrea-salvalaggio <andrea.salvalaggio95@gmail.com>
2022-06-21 15:25:47 +0200

    ciclo while e stampa completata

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-//Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+// Consegna:
+// Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
 
+
+// Creo un array con la lista della spesa
 const list = ['carne', 'pollo', 'uva', 'fagioli', 'olio', 'cipolla'];
 
+// Importo il conteniore della lista dall'HTML
 const cart = document.getElementById('cart-item');
-console.log(cart);
+
+// Inizializzo la variabile
+let i = 0;
+
+
+// Ciclo while che stampa gli elementi dell'array
+while (i < list.length) {
+    const cartElement = document.createElement('li');
+    cartElement.innerHTML = list[i];
+
+    cartElement.classList.add('p-3');
+
+    cart.append(cartElement);
+
+    i++;
+}
